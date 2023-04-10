71c7326e3cc4df4496835da14af371843ff0669f
Eddy <eddy15201@gmail.com>
2023-04-10 20:10:49 +0800

    Add User find_one and update func

--- animal_massage/repository/user_repository.py
+++ animal_massage/repository/user_repository.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from sqlalchemy.orm import Session
 
 from animal_massage.models import User
@@ -5,16 +7,19 @@ from animal_massage.repository.database import with_session
 
 
 @with_session
-def create_user(user: User, session: Session):
+def create_user(user: User, session: Session) -> Optional[User]:
     db_user = User(name=user.name, phone=user.phone, birthday=user.birthday)
     session.add(db_user)
-    print(db_user)
     return db_user
 
 
-if __name__ == "__main__":
-    import datetime
+@with_session
+def find_one_user(user_id: int, session: Session) -> Optional[User]:
+    return session.query(User).filter(User.id == user_id).first()
 
-    create_user(
-        User(name="eddy", phone="0929-111-123", birthday=datetime.date(1997, 8, 8))
+
+@with_session
+def update_user(user_id: int, user: User, session: Session):
+    session.query(User).filter(User.id == user_id).update(
+        {User.name: user.name, User.phone: user.phone, User.birthday: user.birthday}
     )
--- tests/test_crud_with_psy2.py
+++ tests/test_crud_with_psy2.py
@@ -2,19 +2,53 @@ import datetime
 import unittest
 
 from animal_massage.models import User
-from animal_massage.repository.user_repository import create_user
+
+# isort: off
+from animal_massage.repository.user_repository import (
+    create_user,
+    find_one_user,
+    update_user,
+)
+
+# isort: on
+
 from tests.base_testcontainer import BasePGTestContainer
 
 
 class TestUserCRUD(BasePGTestContainer):
-    def test_create_user(self):
-        name = "eddy"
-        phone = "0911-111-111"
-        birthday = datetime.date(1977, 1, 1)
-        user = create_user(
-            User(name=name, phone=phone, birthday=birthday), session=self.session
-        )
+    user_id: int = 1
+    name: str = "eddy"
+    phone: str = "0911-111-111"
+    birthday: datetime.date = datetime.date(1977, 1, 1)
 
+    def test_create_user(self):
+        user = create_user(
+            User(name=self.name, phone=self.phone, birthday=self.birthday),
+            session=self.session,
+        )
+        self.session.commit()
+        self.assert_user(user, self.user_id, self.name, self.phone, self.birthday)
+
+    def test_select_user(self):
+        user = find_one_user(self.user_id, session=self.session)
+        self.assert_user(user, self.user_id, self.name, self.phone, self.birthday)
+
+    def test_update_user(self):
+        _name = "eddy2"
+        _phone = "0911-111-222"
+        _birthday = datetime.date(1999, 1, 1)
+        update_user(
+            user_id=self.user_id,
+            user=User(name=_name, phone=_phone, birthday=_birthday),
+            session=self.session,
+        )
+        user = find_one_user(self.user_id, session=self.session)
+        self.assert_user(user, self.user_id, _name, _phone, _birthday)
+
+    def assert_user(
+        self, user: User, user_id: int, name: str, phone: str, birthday: datetime.date
+    ):
+        self.assertEqual(user.id, user_id)
         self.assertEqual(user.name, name)
         self.assertEqual(user.phone, phone)
         self.assertEqual(user.birthday, birthday)
